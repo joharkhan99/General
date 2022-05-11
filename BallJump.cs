d1e836c0e1e32c73419ae0f930ce5ef1fe370573
single-byte <70197335+single-byte@users.noreply.github.com>
2022-05-11 08:25:09 +0500

    Add files via upload

---
+++ BallJump.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallJump : MonoBehaviour
+{
+    Rigidbody rb;
+    public float jumpSpeed;
+
+    void Start()
+    {
+        jumpSpeed = 10f;
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Ground")
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+    }
+}
