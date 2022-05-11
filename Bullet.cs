bf100e5efae27514b64e58605af1745db4f12327
single-byte <70197335+single-byte@users.noreply.github.com>
2022-05-11 07:42:46 +0500

    Initial commit

---
+++ Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float speed=10f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bulet = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
+            bulet.GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+            Destroy(bulet, 2f);
+        }
+    }
+}
