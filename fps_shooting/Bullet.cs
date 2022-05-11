36497be4056ee6b7ae4ac416122ea7fe111953b9
single-byte <70197335+single-byte@users.noreply.github.com>
2022-05-11 09:19:37 +0500

    Add files via upload

---
+++ fps_shooting/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float speed;
+    private void Start()
+    {
+        speed = 1000f;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GameObject bulet = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
+            bulet.GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+            Destroy(bulet, 2f);
+        }
+    }
+}
---
+++ fps_shooting/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+
+    private void Start()
+    {
+        speed = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0.0f, -5.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0.0f, 5.0f, 0.0f);
+        }
+    }
+}
