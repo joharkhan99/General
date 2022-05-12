27fc9dbb117ccb7f9ef646c30567cb64a4dde740
single-byte <70197335+single-byte@users.noreply.github.com>
2022-05-12 10:20:05 +0500

    Add files via upload

---
+++ PausePlay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausePlay : MonoBehaviour
+{
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
